2107a479a84b3fb0874909257d4d5e11aa2020fa
Caperz Bay <caperzbay@gmail.com>
2015-02-23 06:21:48 +0000

    Se cambio el task de grunt 'zip' por 'compress' ya que permite agregar mediante un string el 'git tag' de la rama master. Otros cambios en el archivo '.gitignore'

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,41 +3,66 @@
 // ZIP requires using node's `path` module
 var path = require('path');
 
+// GIT OBJECT
+// El objeto GitObj contiene la informacion del ultimo tagname del repo git.
+function GitObj( lastTag ) {
+  this.lastTag = lastTag;
+}
 
 module.exports = function (grunt) {
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     var config = {};
 
-    config['zip'] = {
-      'skip-files': {
-        //
-        router: function (filepath) {
+    config['exec'] = {
+      
+      test: {
+        cmd: "git describe --abbrev=0",
 
-          // Grab the extension
-          var extname = path.extname(filepath);
-          var dirname = path.dirname(filepath);
-          var basename = path.basename(filepath);
-          // SI NO ES un archivo temporal del gedit
-          if ( extname.search(/~$/)===-1 ) {
-            // Remueve el directorio raiz 'src'
-            var archivo = filepath.replace(/^src\//i,"");
-            return archivo;
-          } else {
-            // De otra manera, skip it
-            console.log( "--excluding " + basename );
-            return null;
+        stdout: false,
+        stderr: false,
+        
+        callback: function (error, stdout, stderr) {
+          // recupera la salida del shell y lo guarda en el GitObj
+          // Como el stdout contiene una nueva linea se la elimina del string
+          GitObj = new GitObj ( stdout.replace( /\r?\n|\r/g, "" ) );
+          
+          //grunt.log.write( 'Current GIT TAG: ' + GitObj.lastTag );
+          //grunt.log.write( 'stderr: ' + stderr );
+        
+          if (error !== null) {
+            grunt.log.error('exec error: ' + error);
+          }
+        }
+      },
+      
+    }
+
+    // Config para contrib-compress
+    config['compress'] = {
+      main: {
+        options: {
+          archive: function () {
+            // The global value GitObj.lastTag is set by another task
+            return 'distZip/com_etd-' + GitObj.lastTag + '.zip';
           }
         },
-
-        src: [ 'src/**/*' ],
-        dest: 'distZip/com_etd.zip'
+        // La configuracion siguiente es:
+        // cwd: 'src/' -> esto indica que todos los archivos son relativos a ese directorio y por lo tanto
+        // no se debe agregar dichi directorio al archivo comrpimido
+        // src: ['**/*'] -> busca todos los archivos y todos los subdirectorios
+        // dest: -> no se agrego porque lo que hace es crear un directorio raiz dentro del archivo comprimido con ese nombre
+        // Para agregar el directorio de destino se lo hace en options: { archive: 'distZip/<nombre-archivo>.zip' }
+        files: [
+          {expand: true, cwd: 'src/', src: ['**/*']}
+        ]
       }
     }
+    
 
     grunt.initConfig(config);
 
-    var tasks = [ 'zip' ];
+    var tasks = [ 'exec', 'compress' ];
 
     grunt.registerTask('build', tasks);
 };
